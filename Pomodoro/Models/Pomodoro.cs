2419f62d46ac76b33ed0bd4dcb4ab355c90a9424
Yahya Gilany <yahya.gilany@live.com>
2017-06-18 13:50:47 -0400

    created a todo and a pomodoro classes

---
+++ Pomodoro/Models/Pomodoro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pomodoro.Models
+{
+    public class Pomodoro
+    {
+        // Singelton Class.
+        public List<Todo> Tasks { get; set; }
+        public int CurrentTaskIndex { get; set; }
+        public int CyclesCount { get; set; }
+    }
+}
---
+++ Pomodoro/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Pomodoro.Helpers;
+
+namespace Pomodoro.Models
+{
+    public class Todo
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public int DurationMin { get; set; }
+        public int PomodoroCounter { get; set; }
+        public TimerHelper timer { get; set; }
+    }
+}
